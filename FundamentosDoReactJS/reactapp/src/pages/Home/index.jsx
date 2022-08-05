dc376836be415fa741735021b9a3d66b084ce60d
Celina Takeda <82118386+celinatakeda@users.noreply.github.com>
2022-08-05 08:42:18 -0300

    commit

--- FundamentosDoReactJS/reactapp/src/pages/Home/index.jsx
+++ FundamentosDoReactJS/reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,25 @@
+import React, { useState } from 'react';
 import './styles.css';
 
+import { Card } from '../../components/Card';
+
 export function Home() {
+  const [studentName, setStudentName] = useState('');
+
   return (
-    <>
-      <h1>Lista de Presença</h1> 
-      <input type="text" placeholder="Digite o nome..." />
+    <div className="container">
+      <h1>Nome: {studentName}</h1> 
+      <input 
+        type="text" 
+        placeholder="Digite o nome..." 
+        onChange={e => setStudentName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
-    </>      
+
+      <Card name="Celina" time="10:55:25" />
+      <Card name="Maria" time="11:00:10" />
+      <Card name="João" time="14:30:23" />
+    </div>      
   )
 }
 
